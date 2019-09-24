f4ffc8fd9c6f09c9c4923e0a577bc332a13d9899
Jeremy Bivens <jerbbive@ut.utm.edu>
2019-09-24 10:33:33 -0500

    Final

--- ClassWork6/MainWindow.xaml.cs
+++ ClassWork6/MainWindow.xaml.cs
@@ -27,11 +27,13 @@ namespace ClassWork6
         public MainWindow()
         {
             InitializeComponent();
-            cn = new OleDbConnection("ClassWork6ConnectionString");
-            
+            cn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\ClassWork6.accdb");
+
+
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        //Reads Through the Assets Table
+        private void SeeAssets_Click(object sender, RoutedEventArgs e)
         {
             string query = "select * from Assets";
             OleDbCommand cmd = new OleDbCommand(query, cn);
@@ -40,8 +42,39 @@ namespace ClassWork6
             string data = "";
             while (read.Read())
             {
-                data += read[0].ToString() + "\n";
+                data += read[0].ToString() + " " + read[1].ToString() + " " + read[2].ToString() + "\n";
+
             }
+            data = "All data from Assets Table: \n"+ data;
+            TextBox.Text = data;
+
+            cn.Close();
+            
+        }
+
+        //Text Box 
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        { 
+
+        }
+
+        //Reads Through the Employees Table
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "select * from Employees";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            while (read.Read())
+            {
+                data += read[0].ToString() + " " + read[1].ToString() + " " + read[2].ToString() + "\n";
+
+            }
+            data = "All data from Employees Table: \n" + data;
+            TextBox.Text = data;
+
+            cn.Close();
         }
     }
 }
